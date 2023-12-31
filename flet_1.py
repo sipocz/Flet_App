9be5e811a461c40827db6466778e2d4e2df52750
sipocz <sipoczlaszlo@gmail.com>
2023-12-31 22:39:19 +0100

    Update flet_1.py

--- flet_1.py
+++ flet_1.py
@@ -1,11 +1,16 @@
 import flet as ft
+import flet.canvas as cv
+import math
+stroke_paint = ft.Paint(stroke_width=2, style=ft.PaintingStyle.STROKE)
+fill_paint = ft.Paint(style=ft.PaintingStyle.FILL)
 
 def main(page: ft.Page):
     page.title = "Flet Próba"
     
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment=ft.CrossAxisAlignment.CENTER
-
+    stroke_paint = ft.Paint(stroke_width=2, style=ft.PaintingStyle.STROKE)
+    fill_paint = ft.Paint(style=ft.PaintingStyle.FILL)
     txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
     txt_number2 = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=50)
     txt_szoveg = ft.TextField(value="Hello", text_align=ft.TextAlign.CENTER, width=150)
@@ -26,13 +31,25 @@ def main(page: ft.Page):
         txt_number2.value = str(int(txt_number2.value) + 1)
         page.update()
 
-    
+    cp = cv.Canvas(
+        [
+            cv.Circle(100, 100, 50, stroke_paint),
+            cv.Circle(80, 90, 10, stroke_paint),
+            cv.Circle(84, 87, 5, fill_paint),
+            cv.Circle(120, 90, 10, stroke_paint),
+            cv.Circle(124, 87, 5, fill_paint),
+            cv.Arc(70, 95, 60, 40, 0, math.pi, paint=stroke_paint),
+        ],
+        width=float("inf"),
+        expand=True,
+    )
     
     
     page.add(
         ft.Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur quis nibh vitae purus consectetur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur quis nibh vitae purus consectetur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur quis nibh vitae purus consectetur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur quis nibh vitae purus consectetur facilisis sed vitae ipsum. Quisque faucibus sed nulla placerat sagittis. Phasellus condimentum risus vitae nulla vestibulum auctor. Curabitur scelerisque, nibh eget imperdiet consequat, odio ante tempus diam, sed volutpat nisl erat eget turpis. Sed viverra, diam sit amet blandit vulputate, mi tellus dapibus lorem, vitae vehicula diam mauris placerat diam. Morbi sit amet pretium turpis, et consequat ligula. Nulla velit sem, suscipit sit amet dictum non, tincidunt sed nulla. Aenean pellentesque odio porttitor sagittis aliquam. Nam varius at metus vitae vulputate. Praesent faucibus nibh lorem, eu pretium dolor dictum nec. Phasellus eget dui laoreet, viverra magna vitae, pellentesque diam.",
             width=700,
             height=100,),
+        
         ft.Row(
             [
                 ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
@@ -56,7 +73,7 @@ def main(page: ft.Page):
         
         
     )
-
+    page.add(cp)
     page.add(
         ft.Row(
             [
